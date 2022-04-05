977c6f49bd97cfe3d384ab1b9399e546b7c37b8e
iWOLFSTEIN <mtalha7274@gmail.com>
2022-04-05 21:19:03 +0500

    logout button is added

--- lib/Screens/controller_screen.dart
+++ lib/Screens/controller_screen.dart
@@ -1,6 +1,8 @@
 import 'package:custom_navigation_bar/custom_navigation_bar.dart';
 import 'package:flutter/material.dart';
+import 'package:kids_tracking_app/Constants/networking_objects.dart';
 import 'package:kids_tracking_app/Screens/chats_screen.dart';
+import 'package:kids_tracking_app/Screens/login_screen.dart';
 
 class ControllerScreen extends StatefulWidget {
   ControllerScreen({Key? key}) : super(key: key);
@@ -34,6 +36,25 @@ class _ControllerScreenState extends State<ControllerScreen> {
         children: [
           Container(
             color: Colors.orange,
+            child: Center(
+              child: Container(
+                height: 40,
+                width: 200,
+                color: Colors.blue,
+                child: TextButton(
+                    onPressed: () {
+                      Navigator.pushAndRemoveUntil(context,
+                          MaterialPageRoute(builder: (context) {
+                        return LoginScreen();
+                      }), (route) => false);
+                      googleSignInServices.signOutGoogle();
+                    },
+                    child: Text(
+                      "Logout",
+                      style: TextStyle(color: Colors.white),
+                    )),
+              ),
+            ),
           ),
           ChatsScreen()
         ],
