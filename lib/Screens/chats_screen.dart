4e8dfc5400c78b0e3975c699f901fc31ac894fc6
iWOLFSTEIN <mtalha7274@gmail.com>
2022-05-06 10:08:19 +0500

    individual chat deletion implemented

--- lib/Screens/chats_screen.dart
+++ lib/Screens/chats_screen.dart
@@ -7,6 +7,7 @@ import 'package:kids_tracking_app/Constants/network_objects.dart';
 import 'package:kids_tracking_app/Provider/data_provider.dart';
 import 'package:kids_tracking_app/Screens/all_chat_users_screen.dart';
 import 'package:kids_tracking_app/Screens/conversation_screen.dart';
+import 'package:kids_tracking_app/Services/Firebase/firebase_chat_related_services.dart';
 import 'package:kids_tracking_app/Utils/alerts.dart';
 import 'package:kids_tracking_app/Widgets/app_drawer.dart';
 import 'package:modal_progress_hud_nsn/modal_progress_hud_nsn.dart';
@@ -24,7 +25,8 @@ class ChatsScreen extends StatefulWidget {
 }
 
 class _ChatsScreenState extends State<ChatsScreen>
-    with AutomaticKeepAliveClientMixin<ChatsScreen> {
+// with AutomaticKeepAliveClientMixin<ChatsScreen>
+{
   final animatedListStateKey = GlobalKey<AnimatedListState>();
   bool isDeletingAllChats = false;
   @override
@@ -33,7 +35,8 @@ class _ChatsScreenState extends State<ChatsScreen>
     super.initState();
     // Future.delayed(Duration.zero, () {});
   }
-final GlobalKey<ScaffoldState> _scaffoldKey = GlobalKey<ScaffoldState>();
+
+  final GlobalKey<ScaffoldState> _scaffoldKey = GlobalKey<ScaffoldState>();
   @override
   Widget build(BuildContext context) {
     // var dataProvider = Provider.of<DataProvider>(context);
@@ -151,168 +154,75 @@ final GlobalKey<ScaffoldState> _scaffoldKey = GlobalKey<ScaffoldState>();
             //     ),
             //   ),
             // ),
-            StreamBuilder<QuerySnapshot>(
-                stream: firebaseFirestore
-                    .collection('Messages')
-                    .doc(firebaseAuth.currentUser!.email)
-                    .collection('AllMessages')
-                    .orderBy('timestamp', descending: true)
-                    .snapshots(),
-                builder: (context, snapshot) {
-                  if (!snapshot.hasData) {
-                    return Container(
-                      child: Center(
-                          child: Text(
-                        'Loading chats...',
-                        style: TextStyle(
-                          color: Colors.black.withOpacity(0.4),
-                        ),
-                      )),
-                    );
-                  }
+            SingleChildScrollView(
+          child: StreamBuilder<QuerySnapshot>(
+              stream: firebaseFirestore
+                  .collection('Messages')
+                  .doc(firebaseAuth.currentUser!.email)
+                  .collection('AllMessages')
+                  .orderBy('timestamp', descending: true)
+                  .snapshots(),
+              builder: (context, snapshot) {
+                if (!snapshot.hasData) {
+                  return Container(
+                    child: Center(
+                        child: Text(
+                      'Loading chats...',
+                      style: TextStyle(
+                        color: Colors.black.withOpacity(0.4),
+                      ),
+                    )),
+                  );
+                }
 
-                  var chatsList = [];
-                  var querySnapshot = snapshot.data!.docs;
-                  print(snapshot.data!.docs);
-                  for (var i in querySnapshot) {
-                    var documentSnapshotAllMessages = i.data() as Map;
-                    var widget =
-                        //  Builder(builder: (context) {
-                        //   return
-                        Padding(
-                      padding:
-                          EdgeInsets.symmetric(horizontal: 13, vertical: 4),
-                      child: StreamBuilder<
-                              // DocumentSnapshot,
-                              DocumentSnapshot>(
-                          stream:
-                              //  Tuple2(
-                              // firebaseFirestore
-                              //     .collection('Messages')
-                              //     .doc(firebaseAuth.currentUser!.email)
-                              //     .collection('AllMessages')
-                              //     .doc(i.id)
-                              //     .snapshots(),
-                              firebaseFirestore
-                                  .collection("Users")
-                                  .doc(i.id)
-                                  .snapshots(),
+                List<Widget> chatsList = [];
+                var querySnapshot = snapshot.data!.docs;
+                // print(snapshot.data!.docs);
+                for (var i in querySnapshot) {
+                  var documentSnapshotAllMessages = i.data() as Map;
+                  var widget =
+                      //  Builder(builder: (context) {
+                      //   return
+                      ChatContainerItem(
+                          i: i,
+                          documentSnapshotAllMessages:
+                              documentSnapshotAllMessages);
+                  // });
+                  chatsList.add(widget);
+                }
+                if (listEquals(chatsList, [])) {
+                  return Container(
+                    height: height(context) - (height(context) * 17 / 100),
+                    child: Center(
+                        child: Text(
+                      "No Chats Available",
+                      style: TextStyle(color: Colors.black.withOpacity(0.4)),
+                    )),
+                  );
+                }
+                return Column(
+                  children: chatsList,
+                );
 
-                          // firebaseFirestore
-                          //     .collection('LastMessage')
-                          //     .doc(firebaseAuth.currentUser!.email)
-                          //     .collection('AllChatsLastMessages')
-                          //     .doc(documentSnapshotAllMessages["sentTo"])
-                          //     .snapshots(),
-                          // ),
-                          builder: (context, snapshot) {
-                            if (!snapshot.hasData) {
-                              return Container();
-                            }
-
-                            // var documentSnapshot1 =
-                            //     snapshot.item2.data!.data() as Map;
-                            var documentSnapshot2 =
-                                snapshot.data!.data() as Map;
-
-                            return ListTile(
-                                onTap: () {
-                                  Navigator.push(context,
-                                      MaterialPageRoute(builder: (context) {
-                                    return ConversationScreen(
-                                      conversationEmail: i.id,
-                                      name: documentSnapshot2["name"],
-                                    );
-                                  }));
-                                },
-                                leading: CircleAvatar(
-                                  radius: 21,
-                                  backgroundColor:
-                                      Colors.white.withOpacity(0.0),
-                                  backgroundImage: CachedNetworkImageProvider(
-                                      documentSnapshot2["profilePic"]),
-                                ),
-                                title: Wrap(
-                                  children: [
-                                    Text(documentSnapshot2["name"]),
-                                  ],
-                                ),
-                                subtitle: Text(
-                                  documentSnapshotAllMessages["lastMessage"],
-                                  overflow: TextOverflow.ellipsis,
-                                ),
-                                trailing: StreamBuilder<DocumentSnapshot>(
-                                    stream: firebaseFirestore
-                                        .collection('LastMessage')
-                                        .doc(firebaseAuth.currentUser!.email)
-                                        .collection('AllChatsLastMessages')
-                                        .doc(documentSnapshotAllMessages[
-                                            "sentTo"])
-                                        .snapshots(),
-                                    builder: (context, snapshot) {
-                                      if (!snapshot.hasData) {
-                                        return Container(
-                                          width: 1,
-                                        );
-                                      }
-                                      var documentSnapshot1 =
-                                          snapshot.data!.data() as Map;
-                                      return Opacity(
-                                        opacity: (snapshot.data!.data() == null)
-                                            ? 1.0
-                                            : (documentSnapshot1['lastMessage']
-                                                        .compareTo(
-                                                            documentSnapshotAllMessages[
-                                                                'lastMessage']) !=
-                                                    0)
-                                                ? 1.0
-                                                : 0.0,
-                                        child: CircleAvatar(
-                                          backgroundColor:
-                                              Colors.green.shade200,
-                                          radius: 8,
-                                          child: Center(
-                                            child: Text(
-                                              '1',
-                                              style: TextStyle(
-                                                color: Colors.white,
-                                                fontSize: 12,
-                                              ),
-                                            ),
-                                          ),
-                                        ),
-                                      );
-                                    }));
-                          }),
-                    );
-                    // });
-                    chatsList.add(widget);
-                  }
-                  if (listEquals(chatsList, [])) {
-                    return Container(
-                      child: Center(
-                          child: Text(
-                        "No Chats Available",
-                        style: TextStyle(color: Colors.black.withOpacity(0.4)),
-                      )),
-                    );
-                  }
-                  return AnimatedList(
-                      key: animatedListStateKey,
-                      initialItemCount: chatsList.length,
-                      itemBuilder: (context, index, animation) {
-                        return Column(
-                          children: [
-                            Slidable(
-                                key: GlobalKey(),
-                                endActionPane: actionPane(),
-                                startActionPane: actionPane(),
-                                child: chatsList.elementAt(index)),
-                          ],
-                        );
-                      });
-                }),
-        drawer: AppDrawer(scaffoldKey: _scaffoldKey,),
+                // AnimatedList(
+                //     key: animatedListStateKey,
+                //     initialItemCount: chatsList.length,
+                //     itemBuilder: (context, index, animation) {
+                //       return Column(
+                //         children: [
+                //           Slidable(
+                //               key: GlobalKey(),
+                //               endActionPane: actionPane(),
+                //               startActionPane: actionPane(),
+                //               child: chatsList.elementAt(index)),
+                //         ],
+                //       );
+                //     });
+              }),
+        ),
+        drawer: AppDrawer(
+          scaffoldKey: _scaffoldKey,
+        ),
       ),
     );
   }
@@ -346,7 +256,141 @@ final GlobalKey<ScaffoldState> _scaffoldKey = GlobalKey<ScaffoldState>();
         ]);
   }
 
+  // @override
+  // // TODO: implement wantKeepAlive
+  // bool get wantKeepAlive => true;
+}
+
+class ChatContainerItem extends StatefulWidget {
+  const ChatContainerItem({
+    Key? key,
+    required this.i,
+    required this.documentSnapshotAllMessages,
+  }) : super(key: key);
+
+  final QueryDocumentSnapshot<Object?> i;
+  final Map documentSnapshotAllMessages;
+
   @override
-  // TODO: implement wantKeepAlive
-  bool get wantKeepAlive => true;
+  State<ChatContainerItem> createState() => _ChatContainerItemState();
+}
+
+class _ChatContainerItemState extends State<ChatContainerItem> {
+  bool _isChatItemSelected = false;
+  bool _isDeleting = false;
+  @override
+  Widget build(BuildContext context) {
+    return StreamBuilder<DocumentSnapshot>(
+        stream:
+            firebaseFirestore.collection("Users").doc(widget.i.id).snapshots(),
+        builder: (context, snapshot) {
+          if (!snapshot.hasData) {
+            return Container();
+          }
+
+          var documentSnapshot2 = snapshot.data!.data() as Map;
+
+          return ListTile(
+              enabled: !_isDeleting,
+              selected: _isChatItemSelected,
+              selectedTileColor: Colors.grey.withOpacity(0.4),
+              contentPadding: EdgeInsets.symmetric(horizontal: 29, vertical: 4),
+              onLongPress: () {
+                setState(() {
+                  _isChatItemSelected = !_isChatItemSelected;
+                });
+              },
+              onTap: () {
+                if (_isChatItemSelected) {
+                  setState(() {
+                    _isChatItemSelected = false;
+                  });
+                } else {
+                  Navigator.push(context, MaterialPageRoute(builder: (context) {
+                    return ConversationScreen(
+                      conversationEmail: widget.i.id,
+                      name: documentSnapshot2["name"],
+                    );
+                  }));
+                }
+              },
+              leading: CircleAvatar(
+                radius: 21,
+                backgroundColor: Colors.white.withOpacity(0.0),
+                backgroundImage:
+                    CachedNetworkImageProvider(documentSnapshot2["profilePic"]),
+              ),
+              title: Wrap(
+                children: [
+                  Text(documentSnapshot2["name"]),
+                ],
+              ),
+              subtitle: Text(
+                widget.documentSnapshotAllMessages["lastMessage"],
+                overflow: TextOverflow.ellipsis,
+              ),
+              trailing: (_isChatItemSelected)
+                  ? GestureDetector(
+                      onTap: () async {
+                        try {
+                          setState(() {
+                            _isDeleting = true;
+                          });
+                          await deleteUserChat(userEmail: widget.i.id);
+                          setState(() {
+                            _isDeleting = false;
+                          });
+                        } catch (e) {
+                          setState(() {
+                            _isDeleting = false;
+                          });
+                          print(e.toString());
+                        }
+                      },
+                      child: Icon(Icons.delete))
+                  : StreamBuilder<DocumentSnapshot>(
+                      stream: firebaseFirestore
+                          .collection('LastMessage')
+                          .doc(firebaseAuth.currentUser!.email)
+                          .collection('AllChatsLastMessages')
+                          .doc(widget.documentSnapshotAllMessages["sentTo"])
+                          .snapshots(),
+                      builder: (context, snapshot) {
+                        if (!snapshot.hasData) {
+                          return Container(
+                            width: 1,
+                          );
+                        }
+
+                        var documentSnapshot1;
+                        try {
+                          documentSnapshot1 = snapshot.data!.data() as Map;
+                        } catch (e) {}
+
+                        return Opacity(
+                          opacity: (snapshot.data!.data() == null)
+                              ? 1.0
+                              : (documentSnapshot1['lastMessage'].compareTo(
+                                          widget.documentSnapshotAllMessages[
+                                              'lastMessage']) !=
+                                      0)
+                                  ? 1.0
+                                  : 0.0,
+                          child: CircleAvatar(
+                            backgroundColor: Colors.green.shade200,
+                            radius: 8,
+                            child: Center(
+                              child: Text(
+                                '1',
+                                style: TextStyle(
+                                  color: Colors.white,
+                                  fontSize: 12,
+                                ),
+                              ),
+                            ),
+                          ),
+                        );
+                      }));
+        });
+  }
 }
